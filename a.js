1a3e8dc2711bf3cda9590dcbff8229ee79dd0ec8
eric gan <eric.ganym@gmail.com>
2013-02-25 17:52:32 +0800

    add js

---
+++ a.js
@@ -0,0 +1,1 @@
+setTimeout(function(){var links=document.getElementById('List').children[0].children[1].getElementsByTagName('a');var bookinglink;for(var i=0;i<links.length;i++){if(links[i].className=='rent_startorder'){bookinglink=links[i];break;}}bookinglink.click();},5000);
