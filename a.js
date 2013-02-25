3106d6918d143221c82947b31bd10cd7d0c54c3d
eric gan <eric.ganym@gmail.com>
2013-02-25 17:53:47 +0800

    format

--- a.js
+++ a.js
@@ -1,1 +1,11 @@
-setTimeout(function(){var links=document.getElementById('List').children[0].children[1].getElementsByTagName('a');var bookinglink;for(var i=0;i<links.length;i++){if(links[i].className=='rent_startorder'){bookinglink=links[i];break;}}bookinglink.click();},5000);
+setTimeout(function(){
+	var links=document.getElementById('List').children[0].children[1].getElementsByTagName('a');
+	var bookinglink;
+	for(var i=0;i<links.length;i++){
+		if(links[i].className=='rent_startorder'){
+			bookinglink=links[i];
+			break;
+		}
+	}
+	bookinglink.click();
+},5000);
